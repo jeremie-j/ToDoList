6a8c9aeb6a82588cf5b97ed04b94a5d6cf92ad99
Jérémie <jeremie@Jeremies-MacBook-Air.local>
2021-06-23 09:55:13 +0200

    Vue CLI update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <taskList></taskList>
+  <addTask></addTask>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import taskList from './components/taskList.vue'
+import addTask from './components/addTask.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    taskList,
+    addTask
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./css/index.css"></style>
---
+++ src/components/addTask.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="add-task">
+            <h2>Add task</h2>
+            <div id="add-task">
+                <input placeholder="Title" type="text" v-model='newTaskTitle' class="taskTitle">
+                <textarea cols="30" rows="10" placeholder="Description" v-model='newTaskText'></textarea>
+                <button v-on:click="addTask">Ajouter</button>
+            </div>
+        </div>
+</template>
+
+<script>
+export default ({
+})
+</script>
+
+<style scoped>
+
+</style>
